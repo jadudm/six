c4e777d0f6b472c33b7035a81de93921ac0b5a34
Matt Jadud <matthew.jadud@gsa.gov>
2024-10-15 03:56:06 -0400

    Just needs timers...
    Or, push callbacks.
    
    Either way, this is a good MVP.

--- end_to_end.sh
+++ end_to_end.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-host=$1
-
+all () {
+local host="$1"
 http PUT http://localhost:8080/enqueue/CRAWL host=${host} path=/
-read -p "Press <enter> when crawling is complete." bah
+sleep 10m
 http PUT http://localhost:8080/enqueue/PACK host=${host} type=pack_full
-read -p "Press <enter> when packing is complete." bah
+sleep 10m
 http PUT http://localhost:8080/enqueue/SEARCH host=${host} type=search search-id=searcher
-echo "Run some queries!"
+}
 
-echo http POST http://localhost:8484/search/ terms="something or other"
+all jadud.com
+all www.search.gov
+all www.fac.gov
+all www.cloud.gov
+
+#read -p "Press <enter> when crawling is complete." bah
+#read -p "Press <enter> when packing is complete." bah
+#echo "Run some queries!"
+#echo http POST http://localhost:8484/search/ terms="something or other"
